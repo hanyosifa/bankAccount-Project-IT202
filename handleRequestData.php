8e39ad3aad38b296ba1570dd612fdd13324e3e5d
Sifaw H Amazig <mb784@njit.edu>
2019-10-04 17:58:18 -0400

    Handing Request Data

--- handleRequestData.php
+++ handleRequestData.php
@@ -1,37 +1,50 @@
 <?php
-      echo "<pre>" . var_dump($_GET, true) . "</pre>";
-      if(isset($_GET['name'])){
-              echo "<br>Hello, " . $_GET['name'] . "<br>";
-      }
-      if(isset($_GET['number'])){
-	            $number = $_GET['number'];
-             	echo "<br>" . $number . " should be a number...";
-	            echo "<br>but it might not be<br>";
 
-              
-      }
-      if(isset($_GET['number1'])){
-	            $number1 = $_GET['number1'];         
-             	echo "<br>" . $number1 . " should be a numbers...";
-	            echo "<br>but it might not be<br>";
-      }
-               
-             	echo "<br>" . $_GET['name'] . $number . $number1 . ". this is a concatenation of the prev parameter's values...";
-	            echo "<br>but it might a mix of characters and nums<br>";
-         
-         // Question 3 
-              echo "<br>3) Try passing two parameters with the same name but different values, the new value updates the exiting value of the same name ";
-              echo "<br>";
-         // question 4
-              echo "<br>4) Try passing a parameter with a value containing various special characters, the GET function ignores the added 
-              special characters";
-               
-     
-      // TODO
-      // handle addition of 2 or more parameters with proper number parseing
-      // concatenate 2 or more parameter values and echo them
-      // try passing multiple same_named parameters with different values
-      // try passing the parameter value wth special characters
-      // web.njit.edu/~ucid/IT202/handleRequestData.php?parameter1=a&p2=b
+
+      echo "<pre>" . var_export($_GET, true) . "</pre>";
+ 	          if(isset($_GET['name'])){
+               echo "<br>Hello, " . $_GET['name'] . "<br>";
+	}
+ 
+     if(isset($_GET['number'])){
+	          	$number = $_GET['number'];
+	           	echo "<br>" . $number . " should be a number...";
+		          echo "<br>but it might not be<br>";
+	   }            
+  
+echo "<br>1- Adding two or more query parameters with numerical values.<br>";
+     	if(isset($_GET['num1'])){
+             $num1 = $_GET['num1'];
+	}
+	    if(isset($_GET['num2'])){
+ 	        	$num2 = $_GET['num2'];
+	}
+    	if(isset($num1) && isset($num2)){
+		        if(is_numeric($num1) && is_numeric($num2)){
+			          $num1 = (int)$num1;
+		          	$num2 = (int)$num2;
+			          echo "<br>Sum: " . ($num1 + $num2);
+	        	}
+		        else{
+			         echo "<br> Values aren't numbers";
+          }
+ echo "<br><br>2- Concatenate two or more parameter's values and echo them";
+		       echo "<br><br>";
+		       echo "Concat: " . ($num1 . $num2);
+  	}
+	    if(isset($_GET['parameter'])){
+	      	echo "<div> " . $_GET['parameter'] . "</div>";
+	     }
+echo "<br><br>3- Try passing two parameters with the same name but different values, we note that the previous paramer value is updated the new one.";
+      $test = 10;
+      echo "<br>";
+      echo $test1;
+      $test = 2;
+      echo "<br>" . $test;
+
+echo "<br><br>4- Try passing a parameter with a value containing various special characters, we note that add <br>function won't be executed
+ since we have two deffirent data type values. Therefor, the else statment will be printed."; 
+ 
+            
 ?>
       
